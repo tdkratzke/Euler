2459d79915e5d0279f3f3c888cf6b3d1f98cce6b
Thal <tdkratzke@gmail.com>
2022-09-22 22:17:18 -0400

    0112

---
+++ src/com/euler/tdk/euler0112/Euler0112.java
@@ -0,0 +1,59 @@
+/*Working from left-to-right if no digit is exceeded by the digit to its left it is called an increasing number; for example, 134468.
+
+Similarly if no digit is exceeded by the digit to its right it is called a decreasing number; for example, 66420.
+
+We shall call a positive integer that is neither increasing nor decreasing a "bouncy" number; for example, 155349.
+
+Clearly there cannot be any bouncy numbers below one-hundred, but just over half of the numbers below one-thousand (525) are bouncy. In fact, the least number for which the proportion of bouncy numbers first reaches 50% is 538.
+
+Surprisingly, bouncy numbers become more and more common and by the time we reach 21780 the proportion of bouncy numbers is equal to 90%.
+
+Find the least number for which the proportion of bouncy numbers is exactly 99%.*/
+
+//Takes two numbers, first is number of trials, second is percent to hit.
+
+package com.euler.tdk.euler0112;
+import java.util.*;
+
+public class Euler0112 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int test = in.nextInt();
+        for(int a0 = 0; a0 < test; a0++){
+            int percent = in.nextInt();
+
+            int tries = 101;
+            double bouncyCount = 1;
+            while((bouncyCount/tries)*100 < percent){
+                tries += 1;
+                if(isBouncy(tries)){
+                    bouncyCount += 1;
+                }
+            }
+            System.out.println(tries);
+        }
+    }
+
+    public static boolean isBouncy(int num){
+
+        String number = String.valueOf(num);
+        char[] digits = number.toCharArray();
+
+        boolean bouncy = false;
+        boolean up = true;
+        boolean down = true;
+        for(int i = 0; i < digits.length - 1; i++) {
+            if(digits[i] < digits[i+1]){
+                down = false;
+            }else if(digits[i] > digits[i+1]){
+                up = false;
+            }
+            if(!down && !up){
+                bouncy = true;
+                break;
+            }
+        }
+
+        return bouncy;
+    }
+}
