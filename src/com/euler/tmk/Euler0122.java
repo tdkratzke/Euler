2d3cf0d46fe690d67ab38b4eafa17ec67b3b32a8
Thomas Kratzke <tkratzke@gmail.com>
2023-09-24 18:23:42 -0500

    Playing with it, trying to clean it up.

--- src/com/euler/tmk/Euler0122.java
+++ src/com/euler/tmk/Euler0122.java
@@ -8,58 +8,63 @@ import java.util.stream.IntStream;
 public class Euler0122 {
 
 	public static void main(final String[] args) {
-		final int maxPower = 220;
+		final int maxPower = 200;
+
+		/** Initialize the answer vector and which ones we have answers for. */
 		final int[] mValues = new int[maxPower + 1];
-		mValues[0] = 0;
-		mValues[1] = 0;
+		Arrays.fill(mValues, 0, 2, 0);
 		Arrays.fill(mValues, 2, maxPower + 1, maxPower);
 
-		HashSet<BitSet> bitSets = new HashSet<BitSet>();
-		final BitSet trivial = new BitSet(2);
-		trivial.set(1);
-		bitSets.add(trivial);
-
-		final BitSet completed = new BitSet(maxPower + 1);
-		completed.set(1);
+		/** Set the initial byProductsS to be just {1}. */
+		HashSet<BitSet> byProductsS = new HashSet<BitSet>();
+		final BitSet zeroMults = new BitSet(2);
+		zeroMults.set(1);
+		byProductsS.add(zeroMults);
 
 		NMULTS_LOOP : for (int nMults = 1; nMults < maxPower; ++nMults) {
-			final HashSet<BitSet> newBitSets = new HashSet<BitSet>();
-			for (final BitSet bitSet : bitSets) {
+			final HashSet<BitSet> newByProductsS = new HashSet<BitSet>();
+			for (final BitSet byProducts : byProductsS) {
 				/**
-				 * Compute the new powers and, for each one, use it to augment bitSet to form a
-				 * new BitSet of byproducts.
+				 * Compute the new powers and, for each one, use it to augment byProducts to form
+				 * newByProducts.
 				 */
 				final BitSet newPowers = new BitSet();
-				for (int k0 = bitSet.nextSetBit(1); k0 >= 0; k0 = bitSet.nextSetBit(k0 + 1)) {
-					for (int k1 = bitSet.nextSetBit(1); k1 >= 0; k1 = bitSet.nextSetBit(k1 + 1)) {
-						final int k = k0 + k1;
-						if (k > maxPower) {
+				for (int k0 = byProducts.nextSetBit(1); k0 >= 0; k0 = byProducts
+						.nextSetBit(k0 + 1)) {
+					for (int k1 = byProducts.nextSetBit(1); k1 >= 0; k1 = byProducts
+							.nextSetBit(k1 + 1)) {
+						final int power = k0 + k1;
+						if (power > maxPower) {
 							break;
 						}
-						if (!bitSet.get(k)) {
-							newPowers.set(k);
+						if (!byProducts.get(power)) {
+							newPowers.set(power);
 						}
 					}
 				}
-				for (int k = newPowers.nextSetBit(2); k >= 0; k = newPowers.nextSetBit(k + 1)) {
-					final BitSet newBitSet = (BitSet) bitSet.clone();
-					newBitSet.set(k);
-					if (newBitSets.add(newBitSet)) {
-						if (nMults < mValues[k]) {
-							mValues[k] = nMults;
-							completed.set(k);
-							if (completed.nextClearBit(1) == maxPower + 1) {
-								break NMULTS_LOOP;
+				NewPower_LOOP : for (int newPower = newPowers.nextSetBit(
+						2); newPower >= 0; newPower = newPowers.nextSetBit(newPower + 1)) {
+					final BitSet newByProducts = (BitSet) byProducts.clone();
+					newByProducts.set(newPower);
+					if (newByProductsS.add(newByProducts)) {
+						if (mValues[newPower] == maxPower) {
+							mValues[newPower] = nMults;
+							for (int k = 1; k <= maxPower; ++k) {
+								if (mValues[k] == maxPower) {
+									continue NewPower_LOOP;
+								}
 							}
+							break NMULTS_LOOP;
 						}
 					}
 				}
 			}
-			bitSets = newBitSets;
+			byProductsS = newByProductsS;
 		}
 
 		for (int k = 1; k <= maxPower; ++k) {
-			System.out.printf("%sk[%d] mValue[%d]", k > 1 ? "\n" : "", k, mValues[k]);
+			System.out.printf("%sk[%d] mValue[%d]", k == 1 ? "" : (k % 5 == 1 ? "\n\n" : "\n"),
+					k, mValues[k]);
 		}
 		final int sum = IntStream.of(mValues).sum();
 		System.out.printf("\n\nsum[%d]", sum);
